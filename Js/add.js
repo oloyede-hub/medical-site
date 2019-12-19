535d3d03b27f078cfe2525bd13c5a0622333c535
Oloyede Sodiq <sodiqoloyede295@gmail.com>
2019-12-19 10:50:17 +0100

    Initial commit

---
+++ Js/add.js
@@ -0,0 +1,6 @@
+var msg = 'You are offered with 30% off the normal price';
+function updateMessage(){
+    var el = document.getElementById('message');
+    el.textContent = msg;
+}
+updateMessage();
